9ef84f3b73224527a6d44f274bbd3652c868b655
CenkCTI <cenk.mcan.polat@proton.me>
2026-08-13 23:57:37 +0200

    node5c register package advisory adapter

--- src/sources/node5-adapters.ts
+++ src/sources/node5-adapters.ts
@@ -1,3 +1,6 @@
 import type { SourceAdapter } from "../contracts/source.js";
+import { createPackageAdvisoryAdapter } from "./package-advisory.js";
 
-export const node5Adapters: SourceAdapter[] = [];
+export const node5Adapters: SourceAdapter[] = [
+  createPackageAdvisoryAdapter(),
+];
