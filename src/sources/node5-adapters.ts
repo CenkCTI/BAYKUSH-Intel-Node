768ce5aef09d7d6ff3db279f4d0d97de11928eb2
CenkCTI <cenk.mcan.polat@proton.me>
2026-08-14 09:33:44 +0200

    accept official ATT&CK raw JSON transport metadata

--- src/sources/node5-adapters.ts
+++ src/sources/node5-adapters.ts
@@ -6,9 +6,18 @@ import { createMitreAttackEnterpriseAdapter } from "./mitre-attack-enterprise.js
 import { createNode5PackageSource } from "./node5-package-source.js";
 import { createSiemensProductCertAdapter } from "./siemens-productcert.js";
 
+const mitreRawJsonFetch: typeof fetch = async (input, init) => {
+  const response = await fetch(input, init);
+  const requestUrl = input instanceof Request ? new URL(input.url) : new URL(input.toString());
+  if (response.status !== 200 || requestUrl.hostname !== "raw.githubusercontent.com") return response;
+  const headers = new Headers(response.headers);
+  headers.set("content-type", "application/json");
+  return new Response(response.body, { status: response.status, statusText: response.statusText, headers });
+};
+
 export const node5Adapters: SourceAdapter[] = [
   createNode5PackageSource(),
-  createMitreAttackEnterpriseAdapter(),
+  createMitreAttackEnterpriseAdapter({ fetchImpl: mitreRawJsonFetch }),
   createJvnIpediaAdapter(),
   createCisaIcsCsafAdapter(),
   createCertEuPublicationAdapter(),
